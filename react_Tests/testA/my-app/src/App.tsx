e6e3c45acc067ff9bfc7fb56cd099348f7367c6a
StuartMorais <stuartmorais@hotmail.com>
2024-12-11 16:00:42 -0300

    Its broken

--- react_Tests/testA/my-app/src/App.tsx
+++ react_Tests/testA/my-app/src/App.tsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <label htmlFor="inputText" className='bg-red-700'>To do List</label>
+
+      <div className="flex w-full max-w-sm items-center space-x-2" id='divText'>
+        <Input type="Text" placeholder="To do" />
+        <Button type="submit">Add</Button>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <h1 className='flex my-3'>To do:</h1>
+
+      <div className='my-4 flex w-full max-w-sm items-center space-x-2'>
+
+        <ul className='list'>
+          <li>
+            <label htmlFor="">
+              <input type="checkbox" name="chkbx" id="chkbx" />
+              list
+            </label>
+            <button type='button'>Delete</button>
+          </li>
+        </ul>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
